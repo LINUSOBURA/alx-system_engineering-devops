08bb6a922178e347f5dff586bdf33ea2c91d9946
LINUSOBURA <linusobura@gmail.com>
2024-01-30 20:49:08 +0300

    matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,21 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(argument)
-	regex = /School+/
-	result = argument.scan(regex)
-
-	if result.any?
-		matched_string = result.flatten
-		puts "#{matched_string.join}$"
-	else
-		puts ""
-	end
-
-end
-if ARGV.empty?
-
-else
-	argument = ARGV[0]
-
-	match_school(argument)
-end
+puts ARGV[0].scan(/School/).join
