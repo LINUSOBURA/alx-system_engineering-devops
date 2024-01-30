bca619c6c8439eb95874503e9ba004d69ce49ada
LINUSOBURA <linusobura@gmail.com>
2024-01-30 18:20:54 +0300

    matching school

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+def match_school(argument)
+	regex = /School+/
+	result = argument.scan(regex)
+
+	if result.any?
+		matched_string = result.flatten
+		puts "#{matched_string.join}$"
+	else
+		puts ""
+	end
+
+end
+if ARGV.empty?
+	puts "provide argument"
+
+else
+	argument = ARGV[0]
+
+	match_school(argument)
+end
