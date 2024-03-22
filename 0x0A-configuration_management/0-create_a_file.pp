6c121be96cd1a53804d8f9f2a785921ebf0fc4e1
LINUSOBURA <linusobura@gmail.com>
2024-03-22 22:22:09 +0300

    fix 1 & 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creating a file in /tmp
 
 file { '/tmp/school':
+  content =>  'I Love Puppet'
   mode    =>  '0744',
   owner   =>  www-data,
   group   =>  www-data,
-  content =>  'I Love Puppet'
 }
