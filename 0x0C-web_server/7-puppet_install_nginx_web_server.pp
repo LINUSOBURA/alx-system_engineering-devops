c9678cba9d474c8f49477c43a043abac601254a3
LINUSOBURA <linusobura@gmail.com>
2024-03-26 14:04:06 +0300

    fixed puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,3 @@
-# installing and configuring ngix with puppet
-
-
 # Install Nginx package
 package { 'nginx':
   ensure => installed,
@@ -11,7 +8,21 @@ file { '/etc/nginx/sites-available/default':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
-  content => template('nginx/default.erb'),
+  content => "
+server {
+	listen 80;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html;
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+	error_page 404 /404.html;
+	location /404.html {
+		root /var/www/html;
+		internal;
+	}
+}",
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
@@ -24,67 +35,22 @@ file { '/etc/nginx/sites-enabled/default':
   notify  => Service['nginx'],
 }
 
+# Ensure Nginx returns "Hello World!" for root path /
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+
+# Create 404 page
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+}
+
 # Define Nginx service
 service { 'nginx':
   ensure    => running,
   enable    => true,
   hasstatus => true,
   hasrestart => true,
-}
-
-# Template for Nginx default configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Define the template for Nginx default configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Define a custom template for Nginx configuration
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  content => template('nginx/nginx.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Redirect /redirect_me to the specified URL
-nginx::resource::vhost { 'redirect_me':
-  ensure  => present,
-  www_root => '/var/www/html',
-  rewrite_to_https => true,
-  rewrite_to_https_code => 301,
-  rewrite_rules => [
-    { 'from' => '^/redirect_me$', 'to' => 'https://www.youtube.com/watch?v=QH2-TGUlwu4' },
-  ],
-}
-
-# Ensure Nginx is listening on port 80
-nginx::resource::vhost { 'default':
-  ensure  => present,
-  www_root => '/var/www/html',
-  listen_port => 80,
-  rewrite_rules => [
-    { 'from' => '^/$', 'to' => 'https://www.youtube.com/watch?v=QH2-TGUlwu4' },
-  ],
-}
-
-# Ensure Nginx returns "Hello World!" for root path /
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
 }
