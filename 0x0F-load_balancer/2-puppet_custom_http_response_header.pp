350cf1556f1e0822ad4ca7a23586096ac66cd623
LINUSOBURA <linusobura@gmail.com>
2024-04-01 15:31:22 +0300

    Puppet configuration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,61 +1,57 @@
-# Define a Puppet class for configuring Nginx with a custom header
-class nginx_custom_header {
-
-  # Install Nginx package
-  package { 'nginx':
-    ensure  => installed,
-    require => Apt::Update,
-  }
-
-  # Allow Nginx HTTP through UFW firewall
-  exec { 'Allow Nginx HTTP':
-    command => 'ufw allow "Nginx HTTP"',
-    unless  => 'ufw status | grep "Nginx HTTP"',
-    require => Package['nginx'],
-  }
-
-  # Define index.html and 404.html files
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/var/www/html/404.html':
-    ensure  => file,
-    content => "Ceci n'est pas une page",
-    require => Package['nginx'],
-  }
-
-  # Configure Nginx
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => "
-      server {
-        listen 80;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html;
-        add_header X-Served-By $hostname;
-        location /redirect_me {
-          return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-        error_page 404 /404.html;
-        location /404.html {
-          root /var/www/html;
-          internal;
-        }
-      }",
-    require => Package['nginx'],
-  }
-
-  # Restart Nginx service
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-# Apply the nginx_custom_header class
-include nginx_custom_header
+# Define Nginx configuration file
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  content => "
+server {
+	listen 80;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html;
+  add_header X-Served-By $hostname;
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+	error_page 404 /404.html;
+	location /404.html {
+		root /var/www/html;
+		internal;
+	}
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
+}
+
+# Ensure Nginx returns "Hello World!" for root path /
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+
+# Create 404 page
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+}
+
+# Define Nginx service
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  hasstatus => true,
+  hasrestart => true,
+}
