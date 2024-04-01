53b504402c4a0b1234ab88d511a7d2546b86af6a
LINUSOBURA <linusobura@gmail.com>
2024-04-01 15:53:12 +0300

    Puppet configuration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,45 +1,29 @@
-class { 'nginx': }
+# Custom HTTP header in a nginx server
 
-# Update package list (assuming you have a separate resource for this)
-# exec { 'update_apt': ... }
-
-# Install nginx package
-package { 'nginx': ensure => installed }
-
-# Define Nginx configuration using template
-template { '/etc/nginx/sites-available/default.conf':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => '/path/to/nginx.conf.erb',  # Replace with your template path
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+# update ubuntu server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
 }
-
-# Enable the default site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default.conf',  # Updated target
-  notify => Service['nginx'],
+->
+# install nginx web server on server
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
 }
-
-# Ensure Nginx returns "Hello World!" for root path /
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
+->
+# custom Nginx response header (X-Served-By: hostname)
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
 }
-
-# Create 404 page
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-}
-
-# Define Nginx service
+->
+# start service
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  hasrestart => true,
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
 }
