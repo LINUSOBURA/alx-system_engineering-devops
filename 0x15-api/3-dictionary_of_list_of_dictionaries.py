5b9af110c115ba40bd581c467c95ff66a600c2f1
LINUSOBURA <linusobura@gmail.com>
2024-04-23 02:33:33 +0300

    fix 3-dict of list

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,17 +3,16 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import csv
 import json
-from sys import argv
 from urllib.request import urlopen
 
 if __name__ == "__main__":
+
+    all_tasks = {}
+
     with urlopen(f'https://jsonplaceholder.typicode.com/users') as response:
         user_json = response.read().decode('utf-8')
         user_list = json.loads(user_json)
-
-        result_dict_list = []
 
         for user in user_list:
             employee = user['name']
@@ -30,17 +29,6 @@ if __name__ == "__main__":
                     if task.get('userId') == user['id']:
                         user_tasks.append(task)
 
-                total_tasks = len(user_tasks)
-                completed_tasks = 0
-                for task in user_tasks:
-                    if task.get('completed') is True:
-                        completed_tasks += 1
-
-                tasks_done = []
-                for task in user_tasks:
-                    if task.get('completed') is True:
-                        tasks_done.append(task['title'])
-
                 task_owned_list = []
                 for task in user_tasks:
                     task_completed_status = task.get('completed')
@@ -52,10 +40,12 @@ if __name__ == "__main__":
                     }
                     task_owned_list.append(tasks_owned_dict)
 
-                result_dict_list.append({user_id: task_owned_list})
+                all_tasks[user_id] = task_owned_list
+
+                result_dict = {user_id: all_tasks}
+
+    json_object = json.dumps(result_dict, indent=4)
 
     json_file_path = 'todo_all_employees.json'
-    json_object = json.dumps(result_dict_list, indent=4)
-
     with open(json_file_path, 'w') as json_file:
         json_file.write(json_object)
