2ca26fc63c8c91a612a050443254df04ba033276
LINUSOBURA <linusobura@gmail.com>
2024-05-08 23:04:23 +0300

    Initial commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,5 @@
 
 exec{'fix-wordpress_settings_file' :
       command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-      path => '/usr/local/bin/:/bin/'
-	
+      path    => '/usr/local/bin/:/bin/'
 }
